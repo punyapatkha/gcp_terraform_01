3fbb0d8f9bc518f7e5723fcac9a053a39fe4444a
punyapatkha <71396594+punyapatkha@users.noreply.github.com>
2020-10-25 19:21:43 +0700

    [add] test_1.py

---
+++ test_1.py
@@ -0,0 +1,47 @@
+
+import pandas as pd
+from datetime import datetime
+import sqlalchemy
+import mysql.connector 
+import datetime
+from binance.client import Client
+
+def extract_binance_api(maxdate):
+    timestamp = datetime.datetime.timestamp(maxdate)
+    bars = client.get_historical_klines('BTCUSDT', '1d', str(timestamp), limit=1000)
+    btc_df = pd.DataFrame(bars, columns=['open time','open','high','low','close','volume','close time','quote asset volume','number of trades','taker buy base asset volume','taker buy quote asset volume','ignore'])
+    btc_df.set_index('open time', inplace=True)
+    btc_df.reset_index(inplace= True)
+    btc_df['open time'] = pd.to_datetime(btc_df['open time'],unit='ms')
+    btc_df['close time'] = pd.to_datetime(btc_df['close time'],unit='ms')
+    # (D,s,ms,us,ns)
+    btc_df['update time']= datetime.datetime.now()
+    return btc_df
+
+if __name__ == '__main__':
+    client = Client('iypbDmDLObSoajenxQQogZu22Scg7EROGmSb4KWWfkqIwSyoEl7GfMHrYmdESoUN', 'dQ1X8cz9gr1V10WFPdNzmoOy1RFRDw4ZcegEvPzRtcBrHV0QIy99efPFUH4uLPGl')
+    engine = sqlalchemy.create_engine('mysql://root:binance@34.126.75.181/crypto')
+    db = mysql.connector.connect(
+    host="34.126.75.181",
+    user="root",
+    passwd="binance",
+    db="crypto",autocommit=True)
+    
+    querystring = '''SELECT max(`open time`) from `test_3`'''
+    cursor = db.cursor()
+    cursor.execute(querystring)
+    result = cursor.fetchall()
+    master = pd.DataFrame(result)
+    if master.empty:
+        print("no lasted datetime in datawarehouse")
+        b=datetime.datetime.strptime('2000-01-01 01:01:01','%Y-%m-%d %H:%M:%S')
+
+    else:
+        a = str(master.loc[0,0])
+        print("lasted datetime in datawarehouse is "+a)
+        b=datetime.datetime.strptime(a, '%Y-%m-%d %H:%M:%S')
+    c=extract_binance_api(b+datetime.timedelta(days=1))
+    c.to_sql(con=engine, name='test_3', if_exists='append',index=False)
+    querystring2 = '''DELETE FROM test_3 ORDER BY `open time` DESC LIMIT 1'''
+    cursor.execute(querystring2)
+    cursor.close
