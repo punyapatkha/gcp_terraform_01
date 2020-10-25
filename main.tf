94a69ef5b18bdd94ec32fa2af9eb86c1a0563a8e
punyapatkha <71396594+punyapatkha@users.noreply.github.com>
2020-10-25 15:51:55 +0700

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,94 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+
+provider "google" {
+  version = "3.5.0"
+
+  credentials = file("terraform-293515-4d6d53349361.json")
+
+  project = "terraform-293515"
+  region  = "asia-southeast1"
+  zone    = "asia-southeast1-b"
+}
+// double slash for comment in json
+// resource "google_compute_network" "vpc_network" {
+//  name = "terraform-network"
+// }
+
+
+resource "google_compute_instance" "apps" {
+  count        = 2
+  name         = "apps-${count.index + 1}"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-1804-lts"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
+
+
+
+locals {
+  onprem = ["0.0.0.0/0"]
+}
+
+resource "google_sql_database_instance" "postgres1" {
+  name             = "postgres-instance-terraform-01"
+  database_version = "POSTGRES_11"
+  region = "asia-southeast1"
+  settings {
+    tier = "db-f1-micro"
+
+    ip_configuration {
+      ipv4_enabled = true
+      require_ssl  = false
+      dynamic "authorized_networks" {
+        for_each = google_compute_instance.apps
+        iterator = apps
+
+        content {
+          name  = apps.value.name
+          value = apps.value.network_interface.0.access_config.0.nat_ip
+        }
+      }
+
+      dynamic "authorized_networks" {
+        for_each = local.onprem
+        iterator = onprem
+
+        content {
+          name  = "onprem-${onprem.key}"
+          value = onprem.value
+        }
+      }
+    }
+  }
+}
+
+resource "google_sql_user" "users" {
+  name     = "postgres"
+  instance = google_sql_database_instance.postgres1.name
+  password = "postgres"
+}
+
+resource "google_sql_database" "stock-db" {
+  name     = "stock"
+  charset  = "UTF8"
+  collation= "en_US.UTF8"
+  instance = google_sql_database_instance.postgres1.name
+}
